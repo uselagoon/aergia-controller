0f61a704bbb4875362426fa2cb98f9c552e96a39
Ben Jackson <b@benjackson.email>
2022-03-08 18:34:49 +1100

    tests: logs for pod

--- controller-test.sh
+++ controller-test.sh
@@ -15,8 +15,8 @@ KIND_NAME=chart-testing
 
 tear_down () {
   echo -e "${GREEN}============= TEAR DOWN =============${NOCOLOR}"
-  kubectl get pods --all-namespaces
   kubectl logs $(kubectl get pods -n aergia-backend --no-headers | awk '{print $1}') -c manager
+  kubectl get pods --all-namespaces
   kind delete cluster --name ${KIND_NAME}
 }
 
